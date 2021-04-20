8aaaeef03348767960d2ec4d2a7f475cd58619b6
Marcos <vickkmarcos@gmail.com>
2021-04-20 17:27:27 -0300

    Configuring the app

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from core.models import Cid
-admin.site.register(Cid)
 # Register your models here.
+from import_export import resources
+from core.models import Cid
+from import_export.admin import ImportExportModelAdmin
+class BookResource(resources.ModelResource):
+
+    class Meta:
+        model = Cid
+
+class BookAdmin(ImportExportModelAdmin):
+    resource_class = BookResource
+
+admin.site.register(Cid,BookAdmin)
--- core/models.py
+++ core/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Cid(models.Model):
     codigo = models.CharField(max_length=30)
     descricao = models.CharField(max_length=255)
-
     def __str__(self):
         return self.codigo
 
